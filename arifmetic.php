603253666d7ab31f367584862486426a0994c950
Sergey Voitenko <pb@tiptop.kiev.ua>
2016-03-04 15:34:51 +0200

    Done

---
+++ arifmetic.php
@@ -0,0 +1,6 @@
+<?php
+
+$nums = array(12, 15, 20, 25, 59, 79);
+echo array_sum($nums)/count($nums);
+
+?>
---
+++ factorial.php
@@ -0,0 +1,17 @@
+<?php
+
+function factorial ($n)
+{
+    if($n <= 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return $n * factorial($n - 1);
+    }
+}
+
+echo factorial(4);
+
+?>
